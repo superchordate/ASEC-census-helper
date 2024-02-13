aaa2b8f817518da0c64f020caf95562457451472
bryce <superchordate@gmail.com>
2024-02-12 20:15:32 -0600

    Cleanup code.

--- app/server.R
+++ app/server.R
@@ -1,10 +1,6 @@
-# Define server logic required to draw a histogram
 server = function(input, output, session) {
-  
-  # run files in server/ folder.
-  dofiles = list.files('server', pattern = '[.][Rr]', recursive = TRUE, full.names = TRUE )
-  dofiles = dofiles[order(grepl('/[^.]+/', dofiles))] # put source/ home files first.
+  dofiles = list.files('server', pattern = '[.][Rr]', recursive = TRUE, full.names = TRUE)
+  dofiles = dofiles[order(grepl('/[^.]+/', dofiles))]
   for(i in dofiles) source( i, local = TRUE )
-  rm(i)
-   
+  rm(i)   
 }
