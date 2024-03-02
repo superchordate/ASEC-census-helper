4eeb294da4bf5fae49c72ad28f1b94e977c5ff09
bryce <superchordate@gmail.com>
2024-03-02 12:12:45 -0600

    Add kaggle data format.

--- data/build-data.R
+++ data/build-data.R
@@ -13,49 +13,82 @@ require(testthat)
 begin()
 
 filesat = 'raw-data'
-forkaggle = TRUE
+forkaggle = TRUE # create output for Kaggle: pretty field names and write CSV instead of RDS.
 
 runfolder('scripts')
 
-file.remove(list.files('../app/data', full.names = TRUE))
-
 # create the folder and clear prior data. 
 if(!dir.exists('../app/data')) dir.create('../app/data')
+zipname = glue('asec-clean-{min(household$year)}-{max(household$year)}')
 file.remove(list.files('../app/data', full.names = TRUE))
 
-# save new files, one for each column.
+# save app files.
+qsavem(fields, file = '../app/data/appdata')
 for(i in names(person)) qsave(person[[i]], glue('../app/data/person-{i}'))
 for(i in names(household)) qsave(household[[i]], glue('../app/data/household-{i}'))
 for(i in names(family)) qsave(family[[i]], glue('../app/data/family-{i}'))
 
-# save new files, one for each column.
-qsavem(fields, file = '../app/data/appdata')
+# save zipped data.
+ 
+if(!dir.exists(glue('out/{zipname}'))) dir.create(glue('out/{zipname}'))
+file.remove(list.files(glue('out/{zipname}'), full.names = TRUE))
+  
+if(forkaggle){
 
-# save output zip.
-zipname = 'asec-clean-2019-2023'
-if(!file.exists(glue('out/{zipname}.zip'))){
+  # easier-to-read fields. 
+  fields %<>% 
+    mutate(year = max(household$year)) %>%
+    select(
+      Description = desc,
+      Subtopic = subtopic,
+      Topic = topic,
+      `Record Type` = recordtype,
+      Year = year,
+      ID = field,
+      Type = type,
+      `Sample Values` = sample,
+      `% Complete` = complete,
+      `# Distinct Values` = num_values
+  )
   
-  if(!dir.exists(glue('out/{zipname}'))) dir.create(glue('out/{zipname}'))
-  
-  if(forkaggle){
+  # use pretty names with the ID in parenthesis.
+  fields$Description %<>% str_to_title()
+  fields$Description = cc(fields$Description, ' (', fields$ID, ')')
+
+  prettynames = function(table){
     
-    w(fields, glue('out/{zipname}/fields.csv'))
-    w(family, glue('out/{zipname}/family.csv'))
-    w(household, glue('out/{zipname}/household.csv'))
-    w(person, glue('out/{zipname}/person.csv'))
+    descriptions = sapply(
+      names(get(table)),
+      function(x) if(x %in% fields$ID){
+        fields %>% filter(ID == x, `Record Type` == tools::toTitleCase(table)) %>% pull(Description)
+      } else {
+        x
+    })
     
-  } else {
+    badvals = unique(descriptions[duplicated(descriptions)])
+    if(length(badvals) > 0) stop(glue('prettynames [{table}]: bad values [{cc(badvals, sep = ", ")}].'))
     
-    saveRDS(fields, glue('out/{zipname}/fields.RDS'))
-    saveRDS(family, glue('out/{zipname}/family.RDS'))
-    saveRDS(household, glue('out/{zipname}/household.RDS'))
-    saveRDS(person, glue('out/{zipname}/person.RDS'))
+    return(descriptions)
     
   }
+  names(family) = prettynames('family')
+  names(household) = prettynames('household')
+  names(person) = prettynames('person')
   
-  # we have to move into out/ before we zip to avoid that folder being included in the zip.
-  zip(zipfile = glue('out/{zipname}'), files = list.files(glue('out/{zipname}'), full.names = TRUE), flags = '-r9Xj') # https://stackoverflow.com/questions/51844607/zip-files-without-including-parent-directories
+  # write CSVs.
+  w(fields, glue('out/{zipname}/fields.csv'))
+  w(family, glue('out/{zipname}/family.csv'))
+  w(household, glue('out/{zipname}/household.csv'))
+  w(person, glue('out/{zipname}/person.csv'))
   
-  file.remove(list.files(glue('out/{zipname}'), full.names = TRUE))
+} else {
+  
+  saveRDS(fields, glue('out/{zipname}/fields.RDS'))
+  saveRDS(family, glue('out/{zipname}/family.RDS'))
+  saveRDS(household, glue('out/{zipname}/household.RDS'))
+  saveRDS(person, glue('out/{zipname}/person.RDS'))
   
 }
+  
+# https://stackoverflow.com/questions/51844607/zip-files-without-including-parent-directories
+zip(zipfile = glue('out/{zipname}'), files = list.files(glue('out/{zipname}'), full.names = TRUE), flags = '-r9Xj')
--- kaggle.R
+++ kaggle.R
@@ -1,31 +1,61 @@
 require(easyr)
 require(dplyr)
 require(data.table)
+require(htmltools)
 begin()
 
 # read raw data.
-person = fread('data/raw-data/asec-clean-2019-2023/person.csv')
-#household = fread('data/raw-data/asec-clean-2019-2023/household.csv')
-#family = fread('data/raw-data/asec-clean-2019-2023/family.csv')
-fields = fread('data/raw-data/asec-clean-2019-2023/fields.csv')
+person = fread('data/out/asec-clean-2019-2023/person.csv')
+household = fread('data/out/asec-clean-2019-2023/household.csv')
+family = fread('data/out/asec-clean-2019-2023/family.csv')
+fields = fread('data/out/asec-clean-2019-2023/fields.csv')
 
 # filter to one person's data.
 set.seed(544)
-iPERIDNUM = with(person, sample(x = PERIDNUM[year == 2023], size = 1))
-iperson  = person %>% filter(PERIDNUM == iPERIDNUM)
-#ifamily = family %>% filter(FFPOS == iperson$PF_SEQ, FH_SEQ == iperson$PH_SEQ, year == 2023)
-#ihousehold = household %>% filter(H_SEQ == iperson$PH_SEQ, year == 2023)
+iPERIDNUM = with(person, sample(x = `22-Digit Unique Person Identifier (PERIDNUM)`[year == 2023], size = 1))
+iperson  = person %>% filter(`22-Digit Unique Person Identifier (PERIDNUM)` == iPERIDNUM)
+ifamily = family %>% filter(
+  `Unique Family Identifier. This Field Plus Fh_seq Results In A Unique Family Number For The File. (FFPOS)` == iperson$`Pointer To The Sequence Number Of Family Record In Household (Related Subfamilies Point To Primary Family) (PF_SEQ)`, 
+  `Household Sequence Number. Matches H_seq For Same Household (FH_SEQ)` == iperson$`Household Seq Number (PH_SEQ)`, year == 2023)
+ihousehold = household %>% filter(`Household Sequence Number (H_SEQ)` == iperson$`Household Seq Number (PH_SEQ)`, year == 2023)
 
-# combine info from fields
-dt = iperson %>% 
-  mutate_all(as.character) %>% 
-  melt(id.vars = 'year', variable.name = 'Field') %>%
-  dcast.data.table(Field ~ year)
+# set up a function to clean and display the data:
+showdata = function(x){
+  
+  # combine info from fields
+  dt = x %>% 
+    mutate_all(as.character) %>% 
+    melt(id.vars = 'year', variable.name = 'Description') %>%
+    dcast.data.table(Description ~ year)
+  
+  dt %<>% 
+    left_join(
+      fields %>% filter(`Record Type` == x$recordtype[1]) %>% select(Description, Topic, Subtopic),
+      by = 'Description'
+    ) %>%
+    relocate(Description, Subtopic, Topic) %>%
+    mutate(Description = as.character(Description))
+  
+  # filter to only readable fields. 
+  dt %<>% filter(
+    Topic != 'Record Identifiers', # not needed for display.
+    !grepl('Not In Universe', `2023`), # this information is not useful.
+    Subtopic != 'Allocation Flags' # not intuitive for laypeople.
+  )
 
-dt %<>% inner_join(
-  fields %>% select(Field, Description, Topic, Subtopic),
-  by = 'Field'
-) %>%
-  relocate(Description)
+  # return readable HTML.
+  tags$ul(lapply(split(dt, dt$Topic), function(i){
+    tags$li(
+      i$Topic[1],
+      tags$ul(lapply(split(i, 1:nrow(i)), function(j){
+        tags$li(
+          paste(c(j$Description, j$`2023`), collapse = ': ')
+        )
+      }))
+    )
+  }))
+  
+}
 
-View(dt)
+showdata(iperson)
+
